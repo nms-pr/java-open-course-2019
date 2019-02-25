5fec73e5d9d5523fef1272555ccf50ffd8d41a8e
Maschenko Bogdan 13534/4 <mashchenko.bogdan2016@gmail.com>
2019-02-25 23:29:36 +0300

    Done task1

--- src/main/java/ru/mail/polis/open/task1/FizzBuzz.java
+++ src/main/java/ru/mail/polis/open/task1/FizzBuzz.java
@@ -18,3 +18,29 @@ public interface FizzBuzz {
      */
     void print(int from, int to);
 }
+
+class Task1 implements FizzBuzz {
+
+    @Override
+    public void print(int from, int to) {
+
+        for (int i = from; i <= to; i++) {
+
+            if ((i % 3 == 0) && (i % 5 == 0)){
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Task1 task1 = new Task1();
+        task1.print(1, 100);
+    }
+}
