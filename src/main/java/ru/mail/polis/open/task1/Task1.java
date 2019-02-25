ba436c8e8dd841c2ee6dc22fa3747535c13312a5
Vlad <mobila1997@mail.ru>
2019-02-25 22:31:35 +0300

    Task1 has been done;

---
+++ src/main/java/ru/mail/polis/open/task1/Task1.java
@@ -0,0 +1,24 @@
+package ru.mail.polis.open.task1;
+
+public class Task1 implements FizzBuzz {
+
+    @Override
+    public void print(int from, int to) {
+        for (int i = from; i <= to; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Task1 task = new Task1();
+        task.print(1, 100);
+    }
+}
