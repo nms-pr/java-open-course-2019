69fe3efa60938cae6fbeb39ca30fdc182b0a9909
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-02-26 10:53:03 +0300

    Implementing print method in FizzBuzzGame

--- src/main/java/ru/mail/polis/open/task1/FizzBuzzGame.java
+++ src/main/java/ru/mail/polis/open/task1/FizzBuzzGame.java
@@ -1,4 +1,23 @@
 package ru.mail.polis.open.task1;
 
-public class FizzBuzzGame {
+public class FizzBuzzGame implements FizzBuzz {
+
+    @Override
+    public void print(int from, int to) {
+        for(int i = from; i <= to; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+                continue;
+            }
+            if (i % 3 == 0) {
+                System.out.println("Fizz");
+                continue;
+            }
+            if (i % 5 == 0) {
+                System.out.println("Buzz");
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
 }
