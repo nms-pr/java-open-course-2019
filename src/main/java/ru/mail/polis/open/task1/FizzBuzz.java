499ee40d6e2406671e2a61c4a1b20728c7f730ed
almick179 <almick17933@gmail.com>
2019-02-28 21:14:05 +0300

    changes

--- src/main/java/ru/mail/polis/open/task1/FizzBuzz.java
+++ src/main/java/ru/mail/polis/open/task1/FizzBuzz.java
@@ -16,5 +16,17 @@ public interface FizzBuzz {
      * @param from - с какого числа начинать отсчёт
      * @param to - каким числом заканчивать отсчёт
      */
-    void print(int from, int to);
+    default void print(int from, int to) {
+        for (int i = from; i <= to ; i++) {
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
 }
---
+++ src/main/java/ru/mail/polis/open/task1/Solution.java
@@ -0,0 +1,10 @@
+package ru.mail.polis.open.task1;
+
+public class Solution implements FizzBuzz{
+
+    public static void main(String[] args) {
+        new Solution().print(1, 100);
+    }
+
+
+}
