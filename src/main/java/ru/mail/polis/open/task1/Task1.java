08ce065637458ac0ab1452ecb830b5671019c2de
Rinefica <rinefica@gmail.com>
2019-03-02 12:03:22 +0300

    Done 1 task

---
+++ src/main/java/ru/mail/polis/open/task1/Task1.java
@@ -0,0 +1,34 @@
+package ru.mail.polis.open.task1;
+
+/**
+ * @author rinefica
+ */
+public class Task1 implements FizzBuzz {
+    @Override
+    public void print(int from, int to) {
+        boolean isMultiple;
+
+        for (int i = from; i <= to ; i++) {
+            isMultiple = false;
+
+            if (i % 3 == 0) {
+                System.out.print("Fizz");
+                isMultiple = true;
+            }
+
+            if (i % 5 == 0) {
+                System.out.print("Buzz");
+                isMultiple = true;
+            }
+
+            if (!isMultiple)
+                System.out.println(i);
+            else
+                System.out.print("\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        new Task1().print(1, 100);
+    }
+}
