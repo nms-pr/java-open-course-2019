9889f9dc345b5c9de736f3c366f8f830f7888ade
Arkronzxc <ArkronCherf01@gmail.com>
2019-03-03 22:32:30 +0300

    1st commit

--- src/main/java/ru/mail/polis/open/task1/FizzBuzz.java
+++ src/main/java/ru/mail/polis/open/task1/FizzBuzz.java
@@ -4,17 +4,28 @@ package ru.mail.polis.open.task1;
  * @author mikhail.nechaev
  * Since 25/02/2019
  */
-public interface FizzBuzz {
+interface FizzBuzz {
+    public class BuzzFizz implements FizzBuzz {
+        public void doIt() {
+            int i;
+            String s = "";
+            for (i = 1; i < 101; ++i) {
+                if (i % 3 == 0)
+                    s += "Fizz";
+                else if (i % 5 == 0)
+                    s += "Buzz";
+                else if (i % 3 == 0 && i % 5 == 0)
+                    s += "FizzBuzz";
+                else s += Integer.toString(i);
+                System.out.print(s + "\r");
 
-    /**
-     * Напишите программу, которая выводит на экран числа от 1 до 100.
-     * При этом вместо чисел, кратных трем,
-     *  программа должна выводить слово «Fizz»,
-     *  а вместо чисел, кратных пяти — слово «Buzz».
-     * Если число кратно и 3, и 5,
-     *  то программа должна выводить слово «FizzBuzz»
-     * @param from - с какого числа начинать отсчёт
-     * @param to - каким числом заканчивать отсчёт
-     */
-    void print(int from, int to);
+            }
+        }
+
+        public static void main(String[] args) {
+            BuzzFizz buzzFizz = new BuzzFizz();
+            buzzFizz.doIt();
+        }
+
+    }
 }
