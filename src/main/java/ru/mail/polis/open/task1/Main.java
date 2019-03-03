fa83f13ee90e12cc0023e5838076f8f73ee712e6
kirill.njc@gmail.com <kirillsuslov@Sunflower.local>
2019-03-03 23:58:16 +0300

    Homework1

---
+++ src/main/java/ru/mail/polis/open/task1/Main.java
@@ -0,0 +1,11 @@
+package ru.mail.polis.open.task1;
+
+public class Main {
+    static private final int FROM = 1;
+    static private final int TO = 100;
+
+    public static void main(String[] args) {
+        FizzBuzzClass a = new FizzBuzzClass();
+        a.print(FROM, TO);
+    }
+}
