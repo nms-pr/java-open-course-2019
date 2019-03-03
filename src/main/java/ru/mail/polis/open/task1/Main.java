44c0cf3c18b1b151a0887820f8a81dd12279a301
SatykovED <47986860+SatyukovED@users.noreply.github.com>
2019-03-03 20:58:06 +0300

    HomeWork#1

---
+++ src/main/java/ru/mail/polis/open/task1/Main.java
@@ -0,0 +1,10 @@
+package ru.mail.polis.open.task1;
+
+public class Main {
+    static private final int from = 1;
+    static private final int to = 100;
+    public static void main(String[] args) {
+        MyClass myClassTest = new MyClass();
+        myClassTest.print(from, to);
+    }
+}
