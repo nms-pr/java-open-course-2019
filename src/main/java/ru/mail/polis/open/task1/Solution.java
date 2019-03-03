9064f66cd064b4348fd95933a776403f1a9e59c3
Danil Dorogoj <danil_ddd@mail.ru>
2019-03-03 21:39:55 +0300

    Fix Commit

---
+++ src/main/java/ru/mail/polis/open/task1/Solution.java
@@ -0,0 +1,27 @@
+package ru.mail.polis.open.task1;
+
+public class Solution implements FizzBuzz {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.print(1,100);
+    }
+
+    @Override
+    public void print(int from, int to) {
+        for (int i = from;i < to + 1;i++)
+        {
+            if (i % 3 == 0)
+                if (i % 5 == 0)
+                    System.out.println("FizzBuzz");
+                else
+                    System.out.println("Fizz");
+            else
+            if (i % 5 == 0)
+                System.out.println("Buzz");
+            else
+                System.out.println(i);
+        }
+    }
+
+
+}
