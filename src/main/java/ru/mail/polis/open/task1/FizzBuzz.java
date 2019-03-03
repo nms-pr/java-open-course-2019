00be0580c79e2838a622252961388293536e43f1
SamaLamaDoIt <beliashhop@gmail.com>
2019-03-03 14:11:32 +0300

    Hello commit

--- src/main/java/ru/mail/polis/open/task1/FizzBuzz.java
+++ src/main/java/ru/mail/polis/open/task1/FizzBuzz.java
@@ -1,17 +1,30 @@
 package ru.mail.polis.open.task1;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class FizzBuzz implements IFizzBuzz {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите первое число: ");
-        int num_1 = in.nextInt();
-        System.out.println("Введите второе число: ");
-        int num_2 = in.nextInt();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int num_1 = 0, num_2 = 0;
+        try {
+            System.out.println("Введите первое число: ");
+            num_1 = Integer.parseInt(in.readLine());
+
+            System.out.println("Введите второе число: ");
+            num_2 = Integer.parseInt(in.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (NumberFormatException e){
+            System.out.println("Wrong number");
+            e.printStackTrace();
+        }
+
+
         FizzBuzz f = new FizzBuzz();
         f.print(num_1, num_2);
-        in.close();
     }
     @Override
     public void print(int from, int to) {
