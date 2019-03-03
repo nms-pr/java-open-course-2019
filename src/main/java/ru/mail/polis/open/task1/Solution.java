e7874ab7ffbd08b4493e70c2ca36c05d2f59118f
Vera <loozol59@gmail.com>
2019-03-03 19:45:34 +0300

    my first commit

---
+++ src/main/java/ru/mail/polis/open/task1/Solution.java
@@ -0,0 +1,31 @@
+package ru.mail.polis.open.task1;
+
+public class Solution implements FizzBuzz {
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.print(1, 100);
+    }
+
+
+    @Override
+    public void print(int from, int to) {
+        for (int i = from; i < to; i++) {
+        if (i % 3 == 0) {
+            System.out.println("Fizz");
+        }
+        if (i % 5 == 0) {
+            System.out.println("Buzz");
+        } else
+            System.out.println(i);
+    }
+
+    }
+}
+
