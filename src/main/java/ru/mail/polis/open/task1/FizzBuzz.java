3d5b550a5dc901ccf8330c23520f75818e0f81f6
Nikita Kulikov <nekit52331185@yandex.ru>
2019-03-04 18:48:36 +0300

    Fix

--- src/main/java/ru/mail/polis/open/task1/FizzBuzz.java
+++ src/main/java/ru/mail/polis/open/task1/FizzBuzz.java
@@ -23,14 +23,14 @@ public interface FizzBuzz {
 class Change implements FizzBuzz {
     public void print(int from, int to) {
         for (int i = from; i <= to; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0))
+                System.out.println("FizzBuzz");
+            else
             if (i % 3 == 0)
                 System.out.println("Fizz");
             else
             if (i % 5 == 0)
                 System.out.println("Buzz");
-            else
-            if ((i % 3 == 0) && (i % 5 == 0))
-                System.out.println("FizzBuzz");
             else
                 System.out.println(i);
         }
