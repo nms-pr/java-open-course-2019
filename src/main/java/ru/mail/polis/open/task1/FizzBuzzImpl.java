3d87f2ea0bdea50f6526398fc128605330f306d7
ShapovalPaulina <shapovalp799@gmail.com>
2019-03-04 20:32:34 +0300

    commit2

--- src/main/java/ru/mail/polis/open/task1/FizzBuzzImpl.java
+++ src/main/java/ru/mail/polis/open/task1/FizzBuzzImpl.java
@@ -2,12 +2,25 @@ package ru.mail.polis.open.task1;
 
 class FizzBuzzImpl implements FizzBuzz{
 
-    int from;
-    boolean check;
-
     public void print(int from, int to){
-        for (int i = from; i < to; i++){
-            System.out.println(i);
+        boolean check;
+        for (int i = from; i <= to; i++){
+
+            check = false;
+               if (i % 3 == 0){
+                    System.out.print("Fizz");
+                    check = true;
+                }
+                if (i % 5 == 0){
+                    System.out.print("Buzz");
+                    check = true;
+                }
+                if (!check){
+                    System.out.println(i);
+                }
+                else {
+                    System.out.println();
+                }
         }
     }
 }
--- src/main/java/ru/mail/polis/open/task1/Main.java
+++ src/main/java/ru/mail/polis/open/task1/Main.java
@@ -2,33 +2,10 @@ package ru.mail.polis.open.task1;
 
 public class Main {
 
-        public static void main(String[] args){
-            FizzBuzzImpl obj = new FizzBuzzImpl();
-             obj.from = 1;
-
-            // obj.print(1, 100);
-            for (int i = 1;i < 100; i++){
-
-                obj.check = false;
-
-                if (i % 3 == 0){
-                    obj.print(obj.from, i);
-                    obj.from = i+1;
-                    System.out.print("Fizz");
-                    obj.check = true;
-                }
-                if (i % 5 == 0){
-                    if (obj.check != true){
-                        obj.print(obj.from, i);
-                        obj.from = i+1;
-                        obj.check = true;
-                    }
-                    System.out.print("Buzz");
-                }
-                if (obj.check == true){
-                    System.out.println();
-                }
-            }
-            obj.print(obj.from , 101);
+        public static void main(String[] args) {
+            FizzBuzz obj = new FizzBuzzImpl();
+            int from = 1;
+            int to = 100;
+            obj.print(from,to);
         }
 }
