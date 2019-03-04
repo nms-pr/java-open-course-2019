0b9b517aea1743d9a7c639cd1ce18b662098b47a
Arkronzxc <ArkronCherf01@gmail.com>
2019-03-04 18:09:10 +0300

    Hello 2 World Commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -7,7 +7,7 @@ import ru.mail.polis.open.HelloWorld2;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello 2 world!");
         HelloWorld2.main(null);
     }
 }
