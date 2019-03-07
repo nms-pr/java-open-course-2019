683e5e4af6b5f7dd8b3f1a62837b30075a6273e2
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-03-07 12:28:21 +0300

    Creating OutputController interface

---
+++ src/main/java/ru/mail/polis/open/task1/interfaces/OutputController.java
@@ -0,0 +1,15 @@
+package ru.mail.polis.open.task1.interfaces;
+
+/**
+ *
+ * interfaces controlling output method
+ *
+ * @author artyom.petrov
+ * Since 07/03/2019
+ *
+ */
+
+public interface OutputController {
+
+    void print(String s);
+}
