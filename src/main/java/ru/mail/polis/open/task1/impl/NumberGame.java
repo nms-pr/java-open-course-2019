f1710b9d788fb59ea3111bf1761cc5f9d50fa199
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-03-07 13:20:44 +0300

    More refactoring

--- src/main/java/ru/mail/polis/open/task1/impl/NumberGame.java
+++ src/main/java/ru/mail/polis/open/task1/impl/NumberGame.java
@@ -3,7 +3,7 @@ package ru.mail.polis.open.task1.impl;
 import ru.mail.polis.open.task1.interfaces.GameStrategy;
 import ru.mail.polis.open.task1.interfaces.OutputController;
 
-public class NumberGame {
+class NumberGame {
 
     private final GameStrategy strategy;
     private final OutputController outputController;
@@ -13,7 +13,7 @@ public class NumberGame {
         this.outputController = outputController;
     }
 
-    public void run(int from, int to){
+    void run(int from, int to){
         for (int number = from; number <=to; number++) {
             outputController.print(strategy.getCorrespondingString(number));
         }
