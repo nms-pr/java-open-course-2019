b85b89897c6c5a74b040c9d5f7a51c0455dbf32f
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-03-07 11:25:47 +0300

    Creating FizzBuzzGameStrategy class

---
+++ src/main/java/ru/mail/polis/open/task1/Impl/FizzBuzzGameStrategy.java
@@ -0,0 +1,17 @@
+package ru.mail.polis.open.task1.Impl;
+
+import ru.mail.polis.open.task1.Interface.GameStrategy;
+/**
+ *
+ * Implements the rules of FizzBuzz
+ *
+ * @author artyom.petrov
+ * Since 07/03/2019
+ */
+public class FizzBuzzGameStrategy implements GameStrategy {
+
+    @Override
+    public String getCorrespondingString(int number) {
+        return null;
+    }
+}
