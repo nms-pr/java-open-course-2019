7b3d5015e8e48fe9582f03c4d01b9ef77b9b17f4
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-03-07 11:23:00 +0300

    Creating GameStrategy interface

---
+++ src/main/java/ru/mail/polis/open/task1/Interface/GameStrategy.java
@@ -0,0 +1,13 @@
+package ru.mail.polis.open.task1.Interface;
+
+/**
+ *
+ * Interface for getting result from number according to game's rules
+ *
+ * @author artyom.petrov
+ * Since 07/03/2019
+ */
+public interface GameStrategy {
+
+    String getCorrespondingString(int number);
+}
