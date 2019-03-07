921d90f25c9b5c6c7031d2e284341361def8fe28
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-03-07 12:29:47 +0300

    Creating ConsoleOutputController class

---
+++ src/main/java/ru/mail/polis/open/task1/impl/ConsoleOutputController.java
@@ -0,0 +1,10 @@
+package ru.mail.polis.open.task1.impl;
+
+import ru.mail.polis.open.task1.interfaces.OutputController;
+
+public class ConsoleOutputController implements OutputController {
+    @Override
+    public void print(String s) {
+
+    }
+}
