a4350de61e4203de8ac0aecd4fdb622636181314
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-03-07 12:51:37 +0300

    Adding delimiters support for OutputController signatures

--- src/main/java/ru/mail/polis/open/task1/impl/ConsoleOutputController.java
+++ src/main/java/ru/mail/polis/open/task1/impl/ConsoleOutputController.java
@@ -4,7 +4,6 @@ import ru.mail.polis.open.task1.interfaces.OutputController;
 
 public class ConsoleOutputController implements OutputController {
     @Override
-    public void print(String s) {
-
+    public void print(String s, char delimiter) {
     }
 }
