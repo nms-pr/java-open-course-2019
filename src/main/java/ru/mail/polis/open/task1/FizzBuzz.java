fb5fa58e99833cf7bb2c91846aece349246e1867
Maschenko Bogdan 13534/4 <mashchenko.bogdan2016@gmail.com>
2019-03-09 13:28:00 +0300

    update

--- src/main/java/ru/mail/polis/open/task1/FizzBuzz.java
+++ src/main/java/ru/mail/polis/open/task1/FizzBuzz.java
@@ -1,21 +1,12 @@
 package ru.mail.polis.open.task1;
 
 /**
- * @author mikhail.nechaev
- * Since 25/02/2019
+ * @author bogdan.maschenko
+ * Since 08/03/2019
  */
 public interface FizzBuzz {
 
-    /**
-     * Напишите программу, которая выводит на экран числа от 1 до 100.
-     * При этом вместо чисел, кратных трем,
-     *  программа должна выводить слово «Fizz»,
-     *  а вместо чисел, кратных пяти — слово «Buzz».
-     * Если число кратно и 3, и 5,
-     *  то программа должна выводить слово «FizzBuzz»
-     * @param from - с какого числа начинать отсчёт
-     * @param to - каким числом заканчивать отсчёт
-     */
+
     void print(int from, int to) throws NumberMismatchException;
 }
 
@@ -23,7 +14,7 @@ class Task1 implements FizzBuzz {
 
     @Override
     public void print(int from, int to) throws NumberMismatchException {
-        if (from > to) {
+        if ((from != 1) || (to != 100)) {
             throw new NumberMismatchException("Number mismatching");
         }
 
