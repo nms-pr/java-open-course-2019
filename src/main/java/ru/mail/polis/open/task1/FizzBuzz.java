e7271117dec74325e86e7b6f404439c80f2bd30a
Maschenko Bogdan 13534/4 <mashchenko.bogdan2016@gmail.com>
2019-03-11 00:52:56 +0300

    change

--- src/main/java/ru/mail/polis/open/task1/FizzBuzz.java
+++ src/main/java/ru/mail/polis/open/task1/FizzBuzz.java
@@ -7,5 +7,37 @@ package ru.mail.polis.open.task1;
 public interface FizzBuzz {
 
 
-    void print(int from, int to) throws NumberMismatchException;
+    void print(int from, int to);
+}
+
+class Task1 implements FizzBuzz {
+    @Override
+    public void print(int from, int to) {
+        if ((from != 1) || (to != 100)) {
+            throw new IllegalArgumentException("Number mismatching");
+        }
+        for (int i = from; i <= to; i++) {
+
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Task1 task1 = new Task1();
+        try {
+            task1.print(1, 100);
+        } catch (IllegalArgumentException iae) {
+            iae.printStackTrace();
+            System.out.println("Your input data incorrect! Please, will change them");
+        }
+    }
 }
