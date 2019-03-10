3da495e13b832b33c62eabc2b32dfaefc7224f57
GreenBeaver <leonkhvatskin@gmail.com>
2019-03-11 02:12:41 +0300

    Some fixes, a little cleaning

--- src/main/java/ru/mail/polis/open/task1/FizzBuzzClass.java
+++ src/main/java/ru/mail/polis/open/task1/FizzBuzzClass.java
@@ -1,8 +1,16 @@
 package ru.mail.polis.open.task1;
 
 class FizzBuzzClass implements FizzBuzz {
+
+    private static final int UPPER_BOUND = 100;
+    private static final int LOWER_BOUND = 0;
+    private static final int FIZZ_DIVIDER = 3;
+    private static final int BUZZ_DIVIDER = 5;
+
     @Override
     public void print(int from, int to) {
+        if (from < LOWER_BOUND || to > UPPER_BOUND || from > to)
+            throw new IllegalArgumentException();
         StringBuilder out = new StringBuilder();
         for (int i = from; i <= to; i++) {
             if (i % 3 == 0)
--- src/main/java/ru/mail/polis/open/task1/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.mail.polis.open.task1;
-
-public class Main {
-    public static void main(String[] args) {
-        new FizzBuzzClass().print(1, 30);
-    }
-}
