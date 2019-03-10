3e09f3959d51ff74fd52b5b5baee41c18ff86dec
ShapovalPaulina <shapovalp799@gmail.com>
2019-03-10 18:51:17 +0300

    gradle

--- src/main/java/ru/mail/polis/open/task1/Main.java
+++ src/main/java/ru/mail/polis/open/task1/Main.java
@@ -2,10 +2,10 @@ package ru.mail.polis.open.task1;
 
 public class Main {
 
-        public static void main(String[] args) {
-            FizzBuzz obj = new FizzBuzzImpl();
-            int from = 1;
-            int to = 100;
-            obj.print(from,to);
-        }
+    public static void main(String[] args) {
+        FizzBuzz obj = new FizzBuzzImpl();
+        int from = 1;
+        int to = 100;
+        obj.print(from,to);
+    }
 }
