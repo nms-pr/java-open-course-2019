5b6815fdf704ace0cd7326c60f8c584256f43251
Nikita Kulikov <nekit52331185@yandex.ru>
2019-03-11 00:07:30 +0300

    Fix braces

--- src/main/java/ru/mail/polis/open/task1/Change.java
+++ src/main/java/ru/mail/polis/open/task1/Change.java
@@ -5,14 +5,13 @@ public class Change implements FizzBuzz {
         for (int i = from; i <= to; i++) {
             if (i % 15 == 0) {
                 System.out.println("FizzBuzz");
-            }
-            else if (i % 3 == 0) {
+            } else if (i % 3 == 0) {
                 System.out.println("Fizz");
-            }
-            else if (i % 5 == 0) {
+
+            } else if (i % 5 == 0) {
                 System.out.println("Buzz");
-            }
-            else {
+
+            } else {
                 System.out.println(i);
             }
         }
