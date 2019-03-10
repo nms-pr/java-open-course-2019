ed31b8e8eacc52d0f9ceeae358561cf4154163c4
roman <besedin-r-i@yandex.ru>
2019-03-10 20:21:12 +0300

    Первый коммит

---
+++ src/main/java/ru/mail/polis/open/task1/Main.java
@@ -0,0 +1,10 @@
+package ru.mail.polis.open.task1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		FizzBuzz fizzBuzz = new SimpleFizzBuzz();
+		fizzBuzz.print(1, 100);
+	}
+
+}
