17fba40a9e16066693bc025ab9e2c710ce483af7
DMarteenez <dznatok@gmail.com>
2019-03-11 17:34:02 +0300

    Revert "Add files via upload"
    This reverts commit f2695199d81d3c2bced634b32de27a63ee563915.

--- src/ru/mail/polis/open/task1/MainFizzBuzz.java
+++
@@ -1,8 +0,0 @@
-package ru.mail.polis.open.task1;
-
-public class MainFizzBuzz {
-    public static void main(String[] args) {
-        CFizzBuzz fb = new CFizzBuzz();
-        fb.print(1, 100);
-    }
-}
