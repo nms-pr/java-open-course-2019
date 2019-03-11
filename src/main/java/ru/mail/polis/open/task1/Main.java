492bbc03967ad23d22af04a9a15d559bab18bd61
ilya.ivanov <redcrownwork@gmail.com>
2019-03-11 17:36:24 +0300

    First task

---
+++ src/main/java/ru/mail/polis/open/task1/Main.java
@@ -0,0 +1,8 @@
+package ru.mail.polis.open.task1;
+
+public class main {
+    public static void main(String[] args) {
+        FizzBuzzClass var = new FizzBuzzClass();
+        var.print(1,100);
+    }
+}
