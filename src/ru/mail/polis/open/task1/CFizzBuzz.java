f2695199d81d3c2bced634b32de27a63ee563915
Dmitry Martynov <46022207+DMarteenez@users.noreply.github.com>
2019-03-11 17:00:58 +0300

    Add files via upload

---
+++ src/ru/mail/polis/open/task1/CFizzBuzz.java
@@ -0,0 +1,28 @@
+package ru.mail.polis.open.task1;
+
+public class CFizzBuzz implements FizzBuzz {
+    @Override
+    public void print(int from, int to) {
+        StringBuilder s = new StringBuilder();
+
+        for(Integer i = from; i <= to; i++) {
+
+            boolean textIs = false;
+
+            if(i%3 == 0) {
+                s.append("Fizz");
+                textIs = true;
+            }
+
+            if(i%5 == 0) {
+                s.append("Buzz");
+                textIs = true;
+            }
+
+            if(!textIs) s.append(i.toString());
+
+            s.append(System.lineSeparator());
+        }
+        System.out.println(s.toString());
+    }
+}
