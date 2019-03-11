c53128e3f20948e63460d848f5aef32ae47b88c5
Yulia Popova <popova.yua@edu.spbstu.ru>
2019-03-11 18:50:02 +0300

    correction of errors in the first task

--- src/main/java/ru/mail/polis/open/task1/Test.java
+++ src/main/java/ru/mail/polis/open/task1/Test.java
@@ -2,15 +2,19 @@ package ru.mail.polis.open.task1;
 
 class Test implements FizzBuzz{
     public void print(int from, int to) {
-        for (int i = from; i <= to; i++) {
-            if ((i % 3 != 0) && (i % 5 != 0)) {
-                System.out.println(i);
-            } else if ((i % 3 == 0) && (i % 5 != 0)) {
-                System.out.println("Fizz");
-            } else if ((i % 3 != 0) && (i % 5 == 0)) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println("FizzBuzz");
+        if (from > to) {
+            System.out.println("\"from\" must be smaller than \"to\".");
+        } else {
+            for (int i = from; i <= to; i++) {
+                if (i % 15 == 0) {
+                    System.out.println("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                } else {
+                    System.out.println(i);
+                }
             }
         }
     }
