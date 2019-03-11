65f842ccb17662111b4a9204ec2547a299fabfed
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-03-11 20:42:21 +0300

    doing smth

---
+++ src/main/java/ru/mail/polis/open/task1/impl/FizzBuzzGameStrategy.java
@@ -0,0 +1,38 @@
+package ru.mail.polis.open.task1.impl;
+
+import ru.mail.polis.open.task1.interfaces.GameStrategy;
+/**
+ *
+ * Implements the rules of FizzBuzz
+ *
+ * @author artyom.petrov
+ * Since 07/03/2019
+ */
+
+public class FizzBuzzGameStrategy implements GameStrategy {
+
+    private static final int LOWER_BOUND = 0;
+    private static final int UPPER_BOUND = 100;
+
+    @Override
+    public String getCorrespondingString(int number) {
+
+        if ((number < LOWER_BOUND) || (number > UPPER_BOUND)) {
+            throw new IllegalArgumentException("Number must be in range from 0 to 100");
+        }
+
+        if ((number % 15) == 0) {
+            return "FizzBuzz";
+        }
+
+        if ((number % 3) == 0) {
+            return "Fizz";
+        }
+
+        if ((number % 5) == 0) {
+            return "Buzz";
+        }
+
+        return "" + number;
+    }
+}
---
+++ src/test/java/ru/mail/polis/open/task1/impl/ConsoleOutputControllerTest.java
@@ -0,0 +1,50 @@
+package ru.mail.polis.open.task1.impl;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ConsoleOutputControllerTest {
+
+    private static final char DELIMITER = ' ';
+
+    private static OutputStream defaultOutputStream;
+    private static ByteArrayOutputStream testOutputStream;
+    private static ConsoleOutputController outputController;
+
+    @BeforeAll
+    static void initialize() {
+
+        outputController = new ConsoleOutputController(DELIMITER);
+        testOutputStream = new ByteArrayOutputStream();
+        defaultOutputStream = System.out;
+        System.setOut(new PrintStream(testOutputStream));
+    }
+
+    @Test
+    void print() {
+        final String testString1 = "testString!";
+        outputController.print(testString1);
+        assertEquals(testString1 + DELIMITER, testOutputStream.toString());
+        final String testString2 = "Another one!!!";
+        outputController.print(testString2);
+        assertEquals(testString1 + DELIMITER + testString2 + DELIMITER, testOutputStream.toString());
+    }
+
+    @AfterEach
+    void clearBuffer() {
+        testOutputStream.reset();
+    }
+
+    @AfterAll
+    static void restoreOutputStream() {
+        System.setOut(new PrintStream(defaultOutputStream));
+    }
+}
