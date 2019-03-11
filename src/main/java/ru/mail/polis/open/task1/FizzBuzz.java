1eb883ed6e6f5c23ba8ba1573aa61224991573bd
ilya.ivanov <redcrownwork@gmail.com>
2019-03-11 17:59:22 +0300

    Fix Task 2

--- src/main/java/ru/mail/polis/open/task1/FizzBuzz.java
+++ src/main/java/ru/mail/polis/open/task1/FizzBuzz.java
@@ -20,20 +20,3 @@ public interface FizzBuzz {
     void print(int from, int to);
 
 }
-
-class FizzBuzzClass implements FizzBuzz {
-    public void print(int from, int to) {
-        for (int i = from; i <= to; i++) {
-            if (i % 15 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-
-            } else
-                System.out.println(i);
-            }
-        }
-    }
