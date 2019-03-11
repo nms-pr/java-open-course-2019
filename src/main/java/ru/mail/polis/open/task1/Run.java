da8403dd3a5577bc08a40a6a9eda869c670cdccb
Badma <badma3581@gmail.com>
2019-03-11 11:20:37 +0300

    fixed checkstyle

---
+++ src/main/java/ru/mail/polis/open/task1/Run.java
@@ -0,0 +1,22 @@
+package ru.mail.polis.open.task1;
+
+public class Run implements FizzBuzz {
+    public static void main(String[] args) {
+        Run ob = new Run();
+        ob.print(0, 100);
+    }
+
+    public void print(int from, int to) {
+        for (int i = from; i <= to; i++) {
+            if ((i % 3 == 0) & (i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
