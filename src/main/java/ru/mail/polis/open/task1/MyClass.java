4503e2371b57bbb756012f73c563615426997ac7
Alexandra <sashika25@mail.ru>
2019-03-11 23:50:20 +0300

    solution

--- src/main/java/ru/mail/polis/open/task1/MyClass.java
+++ src/main/java/ru/mail/polis/open/task1/MyClass.java
@@ -1,7 +1,7 @@
 package ru.mail.polis.open.task1;
 
 public class MyClass {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         new Counter().print(1,100);
     }
 }
