43363d9589f5b676906528ce808da67b265aa9f8
roman <besedin-r-i@yandex.ru>
2019-03-16 23:52:07 +0300

    Первый коммит

--- src/main/java/ru/mail/polis/open/task3/CorrectBracketSequenceChecker.java
+++ src/main/java/ru/mail/polis/open/task3/CorrectBracketSequenceChecker.java
@@ -1,75 +1,126 @@
 package ru.mail.polis.open.task3;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * Для проверки класса на корректность следует использовать тесты.
- * Команда {@code ./gradlew clean build} должна завершаться корректно.
+ * Для проверки класса на корректность следует использовать тесты. Команда
+ * {@code ./gradlew clean build} должна завершаться корректно.
  * <p>
- * При решении задания следует обратить внимание на использование ключевых слов {@code final} и {@code static}
+ * При решении задания следует обратить внимание на использование ключевых слов {@code final} и
+ * {@code static}
  * <p>
- * В результирующим PR нужно предоставить
- * - Код решения
- * Реализовать все методы
- * Весь код внутри CorrectBracketSequencePredicate
- * Сигнатуры класса, конструктора и метода следует оставить неизменными
- * Комментарии оставить неизменными
- * Можно добавлять дополнительные методы
- * - Тесты
- * Внутри package ru.mail.polis.open.task3
- * В нём будут видны public / protected / package_private методы
+ * В результирующим PR нужно предоставить - Код решения Реализовать все методы Весь код внутри
+ * CorrectBracketSequencePredicate Сигнатуры класса, конструктора и метода следует оставить
+ * неизменными Комментарии оставить неизменными Можно добавлять дополнительные методы - Тесты Внутри
+ * package ru.mail.polis.open.task3 В нём будут видны public / protected / package_private методы
  */
 public final class CorrectBracketSequenceChecker {
+
+    private static Deque<Character> stack;
+
+    private static int successCount;
+    private static int failCount;
+    private static String lastCorrectSequence;
 
     private CorrectBracketSequenceChecker() {
         /* todo: append code if needed */
     }
 
-    /**
-     * Метод проверяющий скобочную последовательность на правильность.
-     * <p>
-     * Пустая строка
-     * — правильная скобочная последовательность.
-     * Правильная скобочная последовательность, взятая в скобки одного типа
-     * — правильная скобочная последовательность.
-     * Правильная скобочная последовательность,
-     * к которой слева или справа приписана правильная скобочная последовательность
-     * — правильная скобочная последовательность.
-     * <p>
-     * Последовательности из больше чем ста символов или с символами не скобок — некорректные.
-     * <p>
-     * Скобки бывают:
-     * 1. Круглые '(', ')'
-     * 2. Квадратные '[', ']'
-     * 3. Фигурные '{', '}'
-     *
-     * @param sequence — входная строка
-     * @return {@code true} — если скобочная последовательность корректна и {@code false} иначе
-     * @throws IllegalArgumentException если в строке содержатся символы, не являющиеся скобками
-     *                                  или если входная строка содержит больше ста символов
-     */
-    public static boolean checkSequence(@Nullable String sequence) {
-        throw new UnsupportedOperationException("todo: implement this");
+    {
+        init();
+    }
+
+    static void init() {
+        successCount = 0;
+        failCount = 0;
+        lastCorrectSequence = null;
     }
 
     /**
-     * Возвращает количество проверок, в результате которых выяснилось,
-     * что входная строка является правильной скобочной последовательностью.
+     * Метод проверяющий скобочную последовательность на правильность.
+     * <p>
+     * Пустая строка — правильная скобочная последовательность. Правильная скобочная
+     * последовательность, взятая в скобки одного типа — правильная скобочная последовательность.
+     * Правильная скобочная последовательность, к которой слева или справа приписана правильная
+     * скобочная последовательность — правильная скобочная последовательность.
+     * <p>
+     * Последовательности из больше чем ста символов или с символами не скобок — некорректные.
+     * <p>
+     * Скобки бывают: 1. Круглые '(', ')' 2. Квадратные '[', ']' 3. Фигурные '{', '}'
+     *
+     * @param sequence — входная строка
+     * @return {@code true} — если скобочная последовательность корректна и {@code false} иначе
+     * @throws IllegalArgumentException если в строке содержатся символы, не являющиеся скобками или
+     *         если входная строка содержит больше ста символов
+     */
+    public static boolean checkSequence(@Nullable String sequence) throws IllegalArgumentException {
+        if (sequence.length() > 100) {
+            failCount++;
+            throw new IllegalArgumentException("Sequence length is more than 100");
+        }
+
+        stack = new ArrayDeque<Character>(sequence.length());
+
+
+        for (int i = 0; i < sequence.length(); i++) {
+            char current = sequence.charAt(i);
+
+            if (isOpenBracket(current)) {
+                stack.push(current);
+            } else if (isCloseBracket(current)) {
+                if (stack.isEmpty() || !isBracketsAnnihilates(current, stack.pop())) {
+                    failCount++;
+                    return false;
+                }
+            } else {
+                failCount++;
+                throw new IllegalArgumentException(current + " not a bracket");
+            }
+        }
+
+        if (stack.isEmpty()) {
+            lastCorrectSequence = sequence;
+            successCount++;
+            return true;
+        } else {
+            failCount++;
+            return false;
+        }
+    }
+
+    public static boolean isOpenBracket(char ch) {
+        return ch == '(' || ch == '[' || ch == '{';
+    }
+
+    public static boolean isCloseBracket(char ch) {
+        return ch == ')' || ch == ']' || ch == '}';
+    }
+
+    public static boolean isBracketsAnnihilates(char current, char previous) {
+        return (current == ')' && previous == '(') || (current == ']' && previous == '[')
+                || (current == '}' && previous == '{');
+    }
+
+    /**
+     * Возвращает количество проверок, в результате которых выяснилось, что входная строка является
+     * правильной скобочной последовательностью.
      *
      * @return количество удачных проверок
      */
     public static int getSuccessChecksCount() {
-        throw new UnsupportedOperationException("todo: implement this");
+        return successCount;
     }
 
     /**
-     * Возвращает количество проверок, в результате которых выяснилось,
-     * что входная строка не является правильной скобочной последовательностью.
+     * Возвращает количество проверок, в результате которых выяснилось, что входная строка не
+     * является правильной скобочной последовательностью.
      *
      * @return количество неудачных проверок
      */
     public static int getFailChecksCount() {
-        throw new UnsupportedOperationException("todo: implement this");
+        return failCount;
     }
 
     /**
@@ -78,6 +129,6 @@ public final class CorrectBracketSequenceChecker {
      * @return последняя правильная скобочная последовательность или null если такой ещё не было
      */
     public static @Nullable String getLastSuccessSequence() {
-        throw new UnsupportedOperationException("todo: implement this");
+        return lastCorrectSequence;
     }
 }
---
+++ src/test/java/ru/mail/polis/open/task3/CorrectBracketSequenceCheckerTest.java
@@ -0,0 +1,166 @@
+package ru.mail.polis.open.task3;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class CorrectBracketSequenceCheckerTest {
+    private static final String[] correctSequences = {
+        "", "()[]{}", "([]){}", "{[()][]({})}", "([{([{}])}])"
+    };
+    private static final String[] incorrectSequences = {
+        ")", "(", "}{}()", "{()[]", "{{}", "([{([{])}])"
+    };
+    private static final char OPEN_PARENTHESIS = '(';
+    private static final char CLOSE_PARENTHESIS = ')';
+    private static final char OPEN_SQUARE_BRACKET = '[';
+    private static final char CLOSE_SQUARE_BRACKET = ']';
+    private static final char OPEN_BRACE = '{';
+    private static final char CLOSE_BRACE = '}';
+
+
+    @Test
+    void positiveIsOpenBracket() {
+        assertTrue(CorrectBracketSequenceChecker.isOpenBracket(OPEN_PARENTHESIS));
+        assertTrue(CorrectBracketSequenceChecker.isOpenBracket(OPEN_SQUARE_BRACKET));
+        assertTrue(CorrectBracketSequenceChecker.isOpenBracket(OPEN_BRACE));
+    }
+
+    @Test
+    void negativeIsOpenBracket() {
+        assertFalse(CorrectBracketSequenceChecker.isOpenBracket(CLOSE_PARENTHESIS));
+        assertFalse(CorrectBracketSequenceChecker.isOpenBracket(CLOSE_SQUARE_BRACKET));
+        assertFalse(CorrectBracketSequenceChecker.isOpenBracket(CLOSE_BRACE));
+        assertFalse(CorrectBracketSequenceChecker.isOpenBracket('太'));
+    }
+
+    @Test
+    void positiveIsCloseBracket() {
+        assertTrue(CorrectBracketSequenceChecker.isCloseBracket(CLOSE_PARENTHESIS));
+        assertTrue(CorrectBracketSequenceChecker.isCloseBracket(CLOSE_SQUARE_BRACKET));
+        assertTrue(CorrectBracketSequenceChecker.isCloseBracket(CLOSE_BRACE));
+    }
+
+    @Test
+    void negativeIsCloseBracket() {
+        assertFalse(CorrectBracketSequenceChecker.isCloseBracket(OPEN_PARENTHESIS));
+        assertFalse(CorrectBracketSequenceChecker.isCloseBracket(OPEN_SQUARE_BRACKET));
+        assertFalse(CorrectBracketSequenceChecker.isCloseBracket(OPEN_BRACE));
+        assertFalse(CorrectBracketSequenceChecker.isCloseBracket('太'));
+    }
+
+    @Test
+    void positiveIsBracketsAnnihilates() {
+        assertTrue(CorrectBracketSequenceChecker.isBracketsAnnihilates(CLOSE_PARENTHESIS,
+                OPEN_PARENTHESIS));
+        assertTrue(CorrectBracketSequenceChecker.isBracketsAnnihilates(CLOSE_SQUARE_BRACKET,
+                OPEN_SQUARE_BRACKET));
+        assertTrue(CorrectBracketSequenceChecker.isBracketsAnnihilates(CLOSE_BRACE, OPEN_BRACE));
+    }
+
+    @Test
+    void negativeIsBracketsAnnihilates() {
+        assertFalse(CorrectBracketSequenceChecker.isBracketsAnnihilates(OPEN_PARENTHESIS,
+                CLOSE_PARENTHESIS));
+        assertFalse(CorrectBracketSequenceChecker.isBracketsAnnihilates(CLOSE_BRACE, '1'));
+        assertFalse(CorrectBracketSequenceChecker.isBracketsAnnihilates('1', OPEN_BRACE));
+    }
+
+    @Test
+    void positiveCheckSequence() {
+        for (String seq : correctSequences) {
+            assertTrue(CorrectBracketSequenceChecker.checkSequence(seq));
+        }
+    }
+
+    @Test
+    void negativeCheckSequence() {
+        for (String seq : incorrectSequences) {
+            assertFalse(CorrectBracketSequenceChecker.checkSequence(seq));
+        }
+    }
+
+    @Test
+    void sequenceShouldBeLessThan100() {
+        int n = 102;
+        char[] chars = new char[n];
+        for (int i = 0; i < n; i++) {
+            chars[i] = i % 2 == 0 ? OPEN_BRACE : CLOSE_BRACE;
+        }
+        String result = new String(chars);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            CorrectBracketSequenceChecker.checkSequence(result);
+        });
+    }
+
+    @Test
+    void sequenceShouldNotContainAnyNonBracketCharacter() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            CorrectBracketSequenceChecker.checkSequence("a");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            CorrectBracketSequenceChecker.checkSequence(OPEN_BRACE + "a");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            CorrectBracketSequenceChecker.checkSequence(correctSequences[2] + "a");
+        });
+    }
+
+    @Test
+    void positiveLastSuccessSequence() {
+        CorrectBracketSequenceChecker.init();
+
+        for (String seq : correctSequences) {
+            CorrectBracketSequenceChecker.checkSequence(seq);
+            assertEquals(seq, CorrectBracketSequenceChecker.getLastSuccessSequence());
+        }
+    }
+
+    @Test
+    void negativeLastSuccessSequence() {
+        CorrectBracketSequenceChecker.init();
+
+        for (String seq : incorrectSequences) {
+            CorrectBracketSequenceChecker.checkSequence(seq);
+            assertNull(CorrectBracketSequenceChecker.getLastSuccessSequence());
+        }
+
+        CorrectBracketSequenceChecker.checkSequence(correctSequences[0]);
+        CorrectBracketSequenceChecker.checkSequence(incorrectSequences[0]);
+        assertEquals(correctSequences[0], CorrectBracketSequenceChecker.getLastSuccessSequence());
+
+        CorrectBracketSequenceChecker.checkSequence(correctSequences[2]);
+        CorrectBracketSequenceChecker.checkSequence(incorrectSequences[3]);
+        assertEquals(correctSequences[2], CorrectBracketSequenceChecker.getLastSuccessSequence());
+    }
+
+    @Test
+    void getSuccessChecksCount() {
+        CorrectBracketSequenceChecker.init();
+
+        for (String seq : correctSequences) {
+            CorrectBracketSequenceChecker.checkSequence(seq);
+        }
+        for (String seq : incorrectSequences) {
+            CorrectBracketSequenceChecker.checkSequence(seq);
+        }
+
+        assertEquals(correctSequences.length,
+                CorrectBracketSequenceChecker.getSuccessChecksCount());
+    }
+
+    @Test
+    void getFailChecksCount() {
+        CorrectBracketSequenceChecker.init();
+
+        for (String seq : incorrectSequences) {
+            CorrectBracketSequenceChecker.checkSequence(seq);
+        }
+
+        assertEquals(0, CorrectBracketSequenceChecker.getSuccessChecksCount());
+    }
+}
