6839de40605237c14ca538e1df27110ccc80786c
SatykovED <47986860+SatyukovED@users.noreply.github.com>
2019-03-19 23:56:35 +0300

    HW-3

--- src/main/java/ru/mail/polis/open/task3/CorrectBracketSequenceChecker.java
+++ src/main/java/ru/mail/polis/open/task3/CorrectBracketSequenceChecker.java
@@ -2,6 +2,9 @@ package ru.mail.polis.open.task3;
 
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.ArrayDeque;
 /**
  * Для проверки класса на корректность следует использовать тесты.
  * Команда {@code ./gradlew clean build} должна завершаться корректно.
@@ -19,11 +22,30 @@ import org.jetbrains.annotations.Nullable;
  * Внутри package ru.mail.polis.open.task3
  * В нём будут видны public / protected / package_private методы
  */
+
 public final class CorrectBracketSequenceChecker {
 
     private CorrectBracketSequenceChecker() {
         /* todo: append code if needed */
     }
+
+    private static final char OPEN_ROUND_BRACE      = '(';
+    private static final char OPEN_SQUARE_BRACE     = '[';
+    private static final char OPEN_FIGURE_BRACE     = '{';
+    private static final char CLOSE_ROUND_BRACE     = ')';
+    private static final char CLOSE_SQUARE_BRACE    = ']';
+    private static final char CLOSE_FIGURE_BRACE    = '}';
+
+    private static final Character[] OPEN_BRACES     = { OPEN_ROUND_BRACE, OPEN_SQUARE_BRACE, OPEN_FIGURE_BRACE };
+    private static final Character[] CLOSE_BRACES    = { CLOSE_ROUND_BRACE, CLOSE_SQUARE_BRACE, CLOSE_FIGURE_BRACE };
+
+    private static int counterOfCorrectTries    = 0;
+    private static int counterOfWrongTries      = 0;
+    private static String lastSuccessSequence;
+
+    private static Deque<Character> stack = new ArrayDeque<>();
+    private static Character prevElementInStack;
+
 
     /**
      * Метод проверяющий скобочную последовательность на правильность.
@@ -49,7 +71,60 @@ public final class CorrectBracketSequenceChecker {
      *                                  или если входная строка содержит больше ста символов
      */
     public static boolean checkSequence(@Nullable String sequence) {
-        throw new UnsupportedOperationException("todo: implement this");
+        //пустая или null строка - правильная
+        if ((sequence == null) || sequence.equals("")) {
+            lastSuccessSequence = sequence;
+            counterOfCorrectTries++;
+            return true;
+        }
+
+        //длина строки больше ста символов
+        if (sequence.length() > 100) {
+            counterOfWrongTries++;
+            throw new IllegalArgumentException("Length of the string should be less than 100");
+        }
+
+        for (int i = 0; i < sequence.length(); i++) {
+            //Проверка на наличие символов, неявляющихся скобками
+            if ((!Arrays.asList(OPEN_BRACES).contains(sequence.charAt(i)))
+                    && (!Arrays.asList(CLOSE_BRACES).contains(sequence.charAt(i)))) {
+                counterOfWrongTries++;
+                stack.clear();
+                throw new IllegalArgumentException("This symbol isn't bracket: " + sequence.charAt(i));
+            }
+
+            //Открытые скобки
+            if (Arrays.asList(OPEN_BRACES).contains(sequence.charAt(i))) {
+                stack.push(sequence.charAt(i));
+            }
+
+            //Проверка закрытых скобок. Последовательность: круглая, квадратная, фигурная
+            for (int currentBrace = 0; currentBrace < 3; currentBrace++) {
+                if (sequence.charAt(i) == CLOSE_BRACES[currentBrace]) {
+                    if (stack.size() == 0) {
+                        counterOfWrongTries++;
+                        return false;
+                    } else {
+                        prevElementInStack = stack.pop();
+                        if (prevElementInStack != OPEN_BRACES[currentBrace]) {
+                            counterOfWrongTries++;
+                            stack.clear();
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (stack.size() != 0) {
+            counterOfWrongTries++;
+            stack.clear();
+            return false;
+        }
+
+        lastSuccessSequence = sequence;
+        counterOfCorrectTries++;
+        return true;
     }
 
     /**
@@ -59,7 +134,7 @@ public final class CorrectBracketSequenceChecker {
      * @return количество удачных проверок
      */
     public static int getSuccessChecksCount() {
-        throw new UnsupportedOperationException("todo: implement this");
+        return counterOfCorrectTries;
     }
 
     /**
@@ -69,7 +144,7 @@ public final class CorrectBracketSequenceChecker {
      * @return количество неудачных проверок
      */
     public static int getFailChecksCount() {
-        throw new UnsupportedOperationException("todo: implement this");
+        return counterOfWrongTries;
     }
 
     /**
@@ -78,6 +153,12 @@ public final class CorrectBracketSequenceChecker {
      * @return последняя правильная скобочная последовательность или null если такой ещё не было
      */
     public static @Nullable String getLastSuccessSequence() {
-        throw new UnsupportedOperationException("todo: implement this");
+        return lastSuccessSequence;
+    }
+
+    public static void reset() {
+        counterOfCorrectTries = 0;
+        counterOfWrongTries = 0;
+        lastSuccessSequence = null;
     }
 }
