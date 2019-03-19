ae8580b3beb2b3d8aba5b9398a952bdcf37d8e37
Don3008 <don3008@yandex.ru>
2019-03-19 19:09:49 +0300

    №4

--- src/test/java/ru/mail/polis/open/task3/CorrectBracketSequenceCheckerTest.java
+++ src/test/java/ru/mail/polis/open/task3/CorrectBracketSequenceCheckerTest.java
@@ -1,13 +1,12 @@
 package ru.mail.polis.open.task3;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static ru.mail.polis.open.task3.CorrectBracketSequenceChecker.checkSequence;
 
-class TestCorrectBracketSequenceChecker {
+class CorrectBracketSequenceCheckerTest {
 
     @AfterEach
     void resetCounters() {
