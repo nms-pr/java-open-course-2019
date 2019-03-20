b6ef2853a17f8bd14368cc3760bda4bc0c388e79
Maschenko Bogdan 13534/4 <mashchenko.bogdan2016@gmail.com>
2019-03-20 17:23:24 +0300

    impl class Sub and SubTest

---
+++ src/test/java/ru/mail/polis/open/task4/SubTest.java
@@ -0,0 +1,19 @@
+package ru.mail.polis.open.task4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SubTest {
+
+    @Test
+    void test() {
+        assertEquals(
+                5,
+                new Sub(
+                        new Const(9),
+                        new Const(4)
+                ).evaluate()
+        );
+    }
+}
