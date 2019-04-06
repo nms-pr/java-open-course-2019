abaffb12f9bad60d34de04a32d1b2ff6138dc802
Petrov Artyom <petrov2.ai@edu.spbstu.ru>
2019-04-06 14:25:03 +0300

    Editing methods signatures in BookProvider interface

--- src/main/java/ru/mail/polis/open/task6/interfaces/BookProvider.java
+++ src/main/java/ru/mail/polis/open/task6/interfaces/BookProvider.java
@@ -1,8 +1,11 @@
 package ru.mail.polis.open.task6.interfaces;
 
 import ru.mail.polis.open.task6.implementation.Book.Book;
+import ru.mail.polis.open.task6.implementation.Book.BookInfo;
 
 public interface BookProvider {
 
-    void lendBook(Book book);
+    BookInfo lendBook(Book book);
+    BookInfo retrieveBook(Book book);
+    Iterable<Book> getAllBooks();
 }
