5e5c48991607447fff5e42b5122ff9f97952c210
Alexandra <sashika25@mail.ru>
2019-04-30 19:56:23 +0300

    маленькие изменения, а я в процессе тестирования

--- src/main/java/ru/mail/polis/open/task9/Rss.java
+++ src/main/java/ru/mail/polis/open/task9/Rss.java
@@ -9,7 +9,6 @@ import com.rometools.rome.io.XmlReader;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Rss {
@@ -17,21 +16,21 @@ public class Rss {
         if(args.length<2) {
             throw new IllegalArgumentException();
         }
+        String separator = System.lineSeparator();
         try{
             URL feedSource = new URL(args[0]);
             SyndFeedInput input = new SyndFeedInput();
             SyndFeed feed = input.build(new XmlReader(feedSource));
             try (FileWriter writer = new FileWriter(args[1],false)){
                 for (SyndEntry entry: feed.getEntries()){
-                    writer.write(entry.getTitle());
-                    writer.append(System.lineSeparator());
-                    writer.write(entry.getDescription().getValue());
-                    writer.append(System.lineSeparator());
-                    writer.write(entry.getLink());
-                    writer.append(System.lineSeparator());
-                    writer.write(entry.getPublishedDate().toString());
-                    writer.append(System.lineSeparator());
-                    writer.append(System.lineSeparator());
+                    writer.write(entry.getTitle()==null ? "null" : entry.getTitle());
+                    writer.append(separator);
+                    writer.write(entry.getDescription().getValue() == null ? "null" : entry.getDescription().getValue());
+                    writer.append(separator);
+                    writer.write(entry.getLink() == null ? "null":entry.getLink());
+                    writer.append(separator);
+                    writer.write(entry.getPublishedDate().toString() == null ? "null" : entry.getPublishedDate().toString());
+                    writer.append(separator);
                 }
             }
         }catch (FeedException e){
--- src/test/java/ru/mail/polis/open/task9/RssTest.java
+++ src/test/java/ru/mail/polis/open/task9/RssTest.java
@@ -6,7 +6,7 @@ public class RssTest {
     @Test
     void test(){
         String[] input = new String[2];
-        input[0]="https://www.fontanka.ru/fontanka.rss";
+        input[0]="file:file.xml";
         input[1]="output.xml";
         Rss.main(input);
     }
