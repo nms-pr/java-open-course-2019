46eb6a29ecd533fefeb28fde69b431f71f582b02
DanilDorogoj <47983995+DanilDorogoj@users.noreply.github.com>
2019-05-16 20:46:29 +0300

    Delete Librarian.java

--- src/main/java/ru/mail/polis/open/task6/Librarian.java
+++
@@ -1,9 +0,0 @@
-package ru.mail.polis.open.task6;
-
-public interface Librarian {
-    Book giveBook(String name, CustomerImpl customer);
-
-    Book giveBook(long id, CustomerImpl customer);
-
-    void putBook(Book book, CustomerImpl customer);
-}
