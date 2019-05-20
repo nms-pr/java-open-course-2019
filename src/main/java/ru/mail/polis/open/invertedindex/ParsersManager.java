52276fd6405d048f191a2dcbd714ba96e45d0eb3
Alexey <medvedev.lasta@yandex.ru>
2019-05-20 17:13:02 +0700

    removing printing stack trace from ParserManager

--- src/main/java/ru/mail/polis/open/invertedindex/ParsersManager.java
+++ src/main/java/ru/mail/polis/open/invertedindex/ParsersManager.java
@@ -38,7 +38,7 @@ public class ParsersManager {
                 }
 
             } catch (Exception ignore) {
-                ignore.printStackTrace();
+
             } finally {
                 futures.pollFirst();
             }
